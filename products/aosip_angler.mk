4e2e81681614e2a09374a429e0da7636cb4953c2
xlxfoxxlx <joshfox87@gmail.com>
2017-05-20 13:53:22 -0400

    Bring in the Dangler
    Change-Id: I282a91605be60d11c118e28a87aa0fb8f6ca2a23

---
+++ products/aosip_angler.mk
@@ -0,0 +1,16 @@
+# Inherit AOSP device configuration for shamu.
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aosip_angler
+PRODUCT_BRAND := Google
+PRODUCT_DEVICE := angler
+PRODUCT_MODEL := Nexus 6P
+PRODUCT_MANUFACTURER := Huawei
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Josh Fox(XlxFoXxlX)" \
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT=google/angler/angler:7.1.2/N2G47O/3852959:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.1.2 N2G47O 3852959 release-keys"
