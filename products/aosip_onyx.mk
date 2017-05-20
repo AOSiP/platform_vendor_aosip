d40ba1a4cea3852cba44fc6742feb4dc369c10d4
ssawrav <s.sawrav@gmail.com>
2017-05-20 11:36:58 +0545

    Products:Add Washing Machine & Onyx
    Change-Id: Ia80ce333b8f19750b4b614c4a07f11037c7042b5

---
+++ products/aosip_onyx.mk
@@ -0,0 +1,37 @@
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from onyx device
+$(call inherit-product, device/oneplus/onyx/device.mk)
+
+# Inherit some common AOSiP stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
+# Call the proprietary setup
+$(call inherit-product-if-exists, vendor/oneplus/onyx/onyx-vendor.mk)
+
+PRODUCT_NAME := aosip_onyx
+PRODUCT_DEVICE := onyx
+PRODUCT_MANUFACTURER := OnePlus
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BRAND := OnePlus
+TARGET_VENDOR := oneplus
+TARGET_VENDOR_PRODUCT_NAME := onyx
+
+PRODUCT_BUILD_PROP_OVERRIDES += DEVICE_MAINTAINERS="Saket Sawrav(I-m-mortal)"
