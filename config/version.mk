7fca6017c40f45b03208f5adbb0097cf793e764d
Akhil Narang <akhilnarang.1999@gmail.com>
2017-05-21 19:40:13 +0200

    vendor: cleanup version var name
    Change-Id: I68fa31a7f04435eabef11e111b97d2923bdc1e77
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -19,9 +19,9 @@ PRODUCT_VERSION_MINOR = 0
 
 AOSIP_BUILDTYPE ?= Derp
 
-ROM_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(AOSIP_BUILDTYPE)-$(CUSTOM_BUILD)-$(shell date -u +%Y%m%d)
+AOSIP_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(AOSIP_BUILDTYPE)-$(CUSTOM_BUILD)-$(shell date -u +%Y%m%d)
 
 PRODUCT_PROPERTY_OVERRIDES += \
-  ro.aosip.version=$(ROM_VERSION) \
+  ro.aosip.version=$(AOSIP_VERSION) \
   ro.aosip.device=$(CUSTOM_BUILD) \
-  ro.modversion=$(ROM_VERSION)
+  ro.modversion=$(AOSIP_VERSION)
