7b34b66a5e07a483f6b47c90e00e532036da0a81
mr-tweaker <aniket.lamba@gmail.com>
2017-05-23 16:45:39 +0530

    devices: Add cancro
    Change-Id: Iba4c67cab88755a818bea1da419cb97918e49ed6

---
+++ products/aosip_cancro.mk
@@ -0,0 +1,36 @@
+# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common AOSiP stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
+# Inherit from cancro device
+$(call inherit-product, device/xiaomi/cancro/cancro.mk)
+
+PRODUCT_NAME := aosip_cancro
+PRODUCT_DEVICE := cancro
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := MI Cancro
+PRODUCT_BUILD_PROP_OVERRIDES += DEVICE_MAINTAINERS="Aniket Lamba"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
