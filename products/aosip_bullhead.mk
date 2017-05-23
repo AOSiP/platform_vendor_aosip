3608c8dd439fb60f3268aed4c84fcf77935ccd48
Omkar <djomi400@gmail.com>
2017-05-23 15:06:23 +0530

    Products: Add bullya
    Change-Id: Ic2f8998c63a1fc50f736b9f085b502fad77762ca

---
+++ products/aosip_bullhead.mk
@@ -0,0 +1,28 @@
+# Inherit AOSP device configuration for bullhead.
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/aosip/config/common.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Setup device specific product configuration.
+
+PRODUCT_NAME := aosip_bullhead
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := bullhead
+PRODUCT_MODEL := Nexus 5X
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Device Maintainer
+
+PRODUCT_BUILD_PROP_OVERRIDES += DEVICE_MAINTAINERS="Omkar Shinde (Megatron007)"
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bullhead \
+    BUILD_FINGERPRINT="google/bullhead/bullhead:7.1.2/N2G47O/3852959:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 7.1.2 N2G47O 3852959 release-keys"
