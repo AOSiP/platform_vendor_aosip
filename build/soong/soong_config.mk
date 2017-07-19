afba9c412b4cd24484887ff4905894572b8f04ca
Wei Wang <wangw@codeaurora.org>
2017-07-19 13:31:58 -0700

    soong: add new soong variable BoardUsesQTIHardware
    read value from BOARD_USES_QTI_HARDWARE
    
    Change-Id: I0c41da5d0a529f8d38ab48abe3ac9a05916da076

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -5,4 +5,6 @@ aosip_soong:
 	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
+	echo '    "BoardUsesQTIHardware":  $(if $(BOARD_USES_QTI_HARDWARE),true,false),'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
