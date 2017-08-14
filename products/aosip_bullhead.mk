4c41ae4890ec34b46cadff9d2fc0ce14ab3f2ce3
Akash Srivastava <akashniki@gmail.com>
2017-08-14 03:44:32 +0000

    - Update Bullhead's build and fingerprint id - Add commit for ViperforAndroid to work in enforcing mode
    Change-Id: If01deb006cd9c0fc0d1bb6d3acac7ff65700e838

--- products/aosip_bullhead.mk
+++ products/aosip_bullhead.mk
@@ -19,10 +19,10 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Device Maintainer
 
-PRODUCT_BUILD_PROP_OVERRIDES += DEVICE_MAINTAINERS="Omkar Shinde (Megatron007)"
+PRODUCT_BUILD_PROP_OVERRIDES += DEVICE_MAINTAINERS="Akash (markakash)"
 
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT="google/bullhead/bullhead:7.1.2/N2G47O/3852959:user/release-keys" \
-    PRIVATE_BUILD_DESC="bullhead-user 7.1.2 N2G47O 3852959 release-keys"
+    BUILD_FINGERPRINT="google/bullhead/bullhead:7.1.2/N2G48C/4104010:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 7.1.2 N2G48C 4104010 release-keys"
--- products/aosip_honami.mk
+++ products/aosip_honami.mk
@@ -1,27 +1,11 @@
-# Device tree for honami for AOSiP
-
-# Inherit AOSP Rhine device parts
-$(call inherit-product, device/sony/honami/aosp_c6903.mk)
-
-# Inherit Lineage Rhine device parts
-$(call inherit-product, device/sony/rhine-common/platform2.mk)
-
-#Inherit Stuff from Vendor
+# Inherit CM common Phone stuff.
 $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
-# Dalvik/HWUI
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+$(call inherit-product, device/sony/honami/full_honami.mk)
 
-PRODUCT_NAME := aosip_honami
-PRODUCT_DEVICE := honami
-PRODUCT_MODEL := Xperia Z1
-PRODUCT_BRAND := Sony
-PRODUCT_MANUFACTURER := Sony
-
-# Fingerprint for honami (from stock)
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/C6903/C6903:5.1.1/14.6.A.1.236/2031203603:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6903-user 5.1.1 14.6.A.1.236 2031203603 release-keys"
 
-PRODUCT_BUILD_PROP_OVERRIDES += DEVICE_MAINTAINERS="Akash Srivastava (markakash)"
+PRODUCT_NAME := aosip_honami
+PRODUCT_DEVICE := honami
