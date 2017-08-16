ff9903213dceac4bacd133698be3e268a9a03380
Luca Stefani <luca020400@lineageos.org>
2017-08-16 21:10:05 +0200

    soong_config: Add LIBART_IMG_BASE variable
    Change-Id: I1f1dde127491670c95dbdbb94bcb50178459b600

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -6,5 +6,6 @@ aosip_soong:
 	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
 	echo '    "BoardUsesQTIHardware":  $(if $(BOARD_USES_QTI_HARDWARE),true,false),'; \
+	echo '    "Libart_img_base": "$(LIBART_IMG_BASE)",'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
