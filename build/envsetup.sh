1b8fee25700dad5a3c2a94aea8608e16417b8e4c
Luca Stefani <luca020400@lineageos.org>
2017-08-17 19:29:00 +0200

    envsetup: Update mka for O
    Change-Id: I36ec10be9152f135ec3645b4304e63ea30d06774

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -407,16 +407,15 @@ unset f
 
 # Make using all available CPUs
 function mka() {
-     local T=$(gettop)
-     if [ "$T" ]; then
-         case `uname -s` in
-             Darwin)
-                  make -C $T -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
-                  ;;
-              *)
- +                mk_timer schedtool -B -n 10 -e ionice -n 7 make -C $T -j$(grep "^processor" /proc/cpuinfo | wc -l) "$@"
-                  ;;
-          esac
+    case `uname -s` in
+        Darwin)
+            m -j "$@"
+            ;;
+        *)
+            mk_timer schedtool -B -n 10 -e ionice -n 7 m -j "$@"
+            ;;
+    esac
+}
 
 # Enable SD-LLVM if available
 if [ -d $(gettop)/prebuilts/snapdragon-llvm/toolchains ]; then
