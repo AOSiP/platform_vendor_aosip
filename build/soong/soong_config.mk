fb301e4d43f6209d4d5363b2804bcdbe52bc5421
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-13 19:06:33 -0400

    aosip/vendor: Expose pathmaps to soong
    Change-Id: I3ba9575c96a116696d1820270e54202b24057a1b

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -10,12 +10,23 @@ aosip_soong:
 	echo '    "QTIAudioPath":  "$(call project-path-for,qcom-audio)",'; \
 	echo '    "QTIDisplayPath":  "$(call project-path-for,qcom-display)",'; \
 	echo '    "QTIMediaPath":  "$(call project-path-for,qcom-media)",';  \
+	echo '    "Uses_non_treble_camera": $(if $(filter true,$(TARGET_USES_NON_TREBLE_CAMERA)),true,false),'; \
+	echo '    "BTVendorPath": "$(call project-path-for,bt-vendor)",'; \
+	echo '    "RILPath": "$(call project-path-for,ril)",'; \
+	echo '    "WLANPath": "$(call project-path-for,wlan)"'; \
 	echo '},'; \
 	echo '"Qualcomm": {'; \
 	echo '    "BoardUsesQTIHardware": $(if $(filter true,$(BOARD_USES_QTI_HARDWARE)),true,false),'; \
 	echo '    "BoardUsesQCOMHardware": $(if $(filter true,$(BOARD_USES_QCOM_HARDWARE)),true,false),'; \
 	echo '    "TargetUsesQCOMBsp": $(if $(filter true,$(TARGET_USES_QCOM_BSP)),true,false),'; \
 	echo '    "TargetUsesQCOMLegacyBsp": $(if $(filter true,$(TARGET_USES_QCOM_LEGACY_BSP)),true,false),'; \
-	echo '    "BoardUsesLegacyAlsa": $(if $(filter true,$(BOARD_USES_LEGACY_ALSA_AUDIO)),true,false)'; \
+	echo '    "BoardUsesLegacyAlsa": $(if $(filter true,$(BOARD_USES_LEGACY_ALSA_AUDIO)),true,false),'; \
+	echo '    "QCOMAudioPath": "$(call project-path-for,qcom-audio)",'; \
+	echo '    "QCOMCameraPath": "$(call project-path-for,qcom-camera)",'; \
+	echo '    "QCOMDataservicesPath": "$(call project-path-for,qcom-dataservices)",';  \
+	echo '    "QCOMDisplayPath": "$(call project-path-for,qcom-display)",';  \
+	echo '    "QCOMGPSPath": "$(call project-path-for,qcom-gps)",';  \
+	echo '    "QCOMMediaPath": "$(call project-path-for,qcom-media)",';  \
+	echo '    "QCOMSensorsPath": "$(call project-path-for,qcom-sensors)"';  \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
