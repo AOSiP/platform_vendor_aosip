efae864759f85f1d36fa6d12b702e9e6d2ce5226
BeansTown106 <nbenis106@gmail.com>
2017-09-16 16:38:38 -0400

    aosp_fixes: filter based on devices with project treble
    Change-Id: Iee9d1d56d5a6226b20cc04e0e7ac096210f9b3a7

--- config/common.mk
+++ config/common.mk
@@ -14,6 +14,7 @@ PRODUCT_COPY_FILES += \
     vendor/aosip/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/aosip/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
 
+ifeq ($(filter aosip_marlin aosip_sailfish,$(TARGET_PRODUCT)),)
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
     dalvik.vm.debug.alloc=0 \
@@ -25,6 +26,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.build.selinux=1
+endif
 
 PRODUCT_COPY_FILES += \
     vendor/aosip/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
