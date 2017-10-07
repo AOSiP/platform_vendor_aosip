BlissRoms/platform_vendor_bliss

vendor/aosp: Add LatinIME dictionaries support
Thanks to CyanogenMod

Signed-off-by: Meninblack007 <sanyam.53jain@gmail.com>

common.mk
@@ -20,6 +20,9 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
 
+# Include LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/aosp/overlay/dictionaries
+
 # init.d support
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/bin/sysinit:system/bin/sysinit \

AOSiP/platform_vendor_aosip

Change-Id: If528a90d316e5cebfb2c68b447678471b70427e4

config/common.mk
@@ -50,6 +50,9 @@ PRODUCT_COPY_FILES +=  \
 # Common overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/aosip/overlay/common
 
+# Include LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/aosip/overlay/dictionaries
+
 # Packages
 include vendor/aosip/config/packages.mk
 