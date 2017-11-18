ed9f9eeb03170ac18948f756d612c05c85349af6
Akhil Narang <akhilnarang.1999@gmail.com>
2017-11-19 00:07:40 +0530

    Make bootanimation copy for all devices
    Change-Id: Ib826c5e89d3dc1a83f8168a478a896eb9ab4da0d
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,12 +1,2 @@
-#ifneq ($(filter aosip_device,$(TARGET_PRODUCT)),)
-#    PRODUCT_COPY_FILES += \
-#        vendor/aosip/prebuilt/common/bootanimation/480.zip:system/media/bootanimation.zip
-#endif
-ifneq ($(filter aosip_oneplus3,$(TARGET_PRODUCT)),)
-    PRODUCT_COPY_FILES += \
-        vendor/aosip/prebuilt/common/bootanimation/720.zip:system/media/bootanimation.zip
-endif
-#ifneq ($(filter aosip_device,$(TARGET_PRODUCT)),)
-#    PRODUCT_COPY_FILES += \
-#        vendor/aosip/prebuilt/common/bootanimation/1080.zip:system/media/bootanimation.zip
-#endif
+PRODUCT_COPY_FILES += \
+    vendor/aosip/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
