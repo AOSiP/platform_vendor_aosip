d399abf4a46c9db910bb8328c4ea5d80361996cb
Dan Pasanen <dan.pasanen@gmail.com>
2017-12-12 15:07:09 -0600

    repopick: use https by default for repopicking
    Change-Id: Iae1fde3515b0377a2b9253102c82d28b0fdaca4a
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- build/tools/repopick.py
+++ build/tools/repopick.py
@@ -123,7 +123,7 @@ def fetch_query(remote_url, query):
 
 if __name__ == '__main__':
     # Default to AOSiP Gerrit
-    default_gerrit = 'http://review.aosiprom.com'
+    default_gerrit = 'https://review.aosiprom.com'
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent('''\
         repopick.py is a utility to simplify the process of cherry picking
