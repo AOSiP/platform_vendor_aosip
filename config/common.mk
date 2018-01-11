CarbonROM/android_vendor_carbon

sdclang: Gaurd usage
Change-Id: Ie3e1c8130a7b10e2a721c66fe247a4cf6e8373a8
Signed-off-by: Joe Maples <joe@frap129.org>

config/common.mk
@@ -91,5 +91,9 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/carbon/overlay/common
 # Squisher Location
 SQUISHER_SCRIPT := vendor/carbon/tools/squisher
 
-# include definitions for SDCLANG
-include device/qcom/common/sdclang/sdclang.mk
+# Include SDCLANG definitions if it is requested and available
+ifeq ($(HOST_OS),linux)
+    ifneq ($(wildcard vendor/qcom/sdclang-3.8/),)
+        include vendor/carbon/sdclang/sdclang.mk
+    endif
+endif

AOSiP/platform_vendor_aosip

config/common.mk
@@ -75,5 +75,9 @@ include vendor/aosip/config/packages.mk
 # Versioning
 include vendor/aosip/config/version.mk
 
-# include definitions for SDCLANG
-include vendor/aosip/sdclang/sdclang.mk
+# Include SDCLANG definitions if it is requested and available
+ifeq ($(HOST_OS),linux)
+    ifneq ($(wildcard vendor/qcom/sdclang-3.8/),)
+        include vendor/aosip/sdclang/sdclang.mk
+    endif
+endif