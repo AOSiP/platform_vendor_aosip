b966bee8ac85cebe8e00e04491c1496783eecc8c
Josh <joshfox87@gmail.com>
2018-03-14 08:22:05 -0400

    vendor/aosip: Clean up our common.mk a bit
    -rearrange a little for my ocd
    -fix prop call for a few
    -remove call for pixel devices
    
    Change-Id: Ide5008a1abe9f578744edbfc4cf902caa43cc66f

--- config/common.mk
+++ config/common.mk
@@ -1,20 +1,5 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
-ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
-PRODUCT_GENERIC_PROPERTIES += \
-    ro.com.google.clientidbase=android-google
-else
-PRODUCT_GENERIC_PROPERTIES += \
-    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
-endif
-
-# Backup Tool
-PRODUCT_COPY_FILES += \
-    vendor/aosip/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/aosip/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/aosip/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
-
-ifeq ($(filter aosip_marlin aosip_sailfish,$(TARGET_PRODUCT)),)
 PRODUCT_GENERIC_PROPERTIES += \
     keyguard.no_require_sim=true \
     dalvik.vm.debug.alloc=0 \
@@ -27,19 +12,21 @@ PRODUCT_GENERIC_PROPERTIES += \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.build.selinux=1 \
     ro.carrier=unknown
+
+ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
+PRODUCT_GENERIC_PROPERTIES += \
+    ro.com.google.clientidbase=android-google
+else
+PRODUCT_GENERIC_PROPERTIES += \
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
-PRODUCT_COPY_FILES += \
-    vendor/aosip/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/aosip/prebuilt/common/bin/sysinit:system/bin/sysinit
-
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images
-
-# Init file
-PRODUCT_COPY_FILES += \
-    vendor/aosip/prebuilt/common/etc/init.local.rc:root/init.aosip.rc
+# Include SDCLANG definitions if it is requested and available
+ifeq ($(HOST_OS),linux)
+    ifneq ($(wildcard vendor/qcom/sdclang-4.0/),)
+        include vendor/aosip/sdclang/sdclang.mk
+    endif
+endif
 
 # LatinIME gesture typing
 ifeq ($(TARGET_ARCH),arm64)
@@ -51,6 +38,25 @@ PRODUCT_COPY_FILES += \
     vendor/aosip/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
     vendor/aosip/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 endif
+
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+# Enable ADB authentication
+PRODUCT_GENERIC_PROPERTIES += ro.adb.secure=1
+endif
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/aosip/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/aosip/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/aosip/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
+
+PRODUCT_COPY_FILES += \
+    vendor/aosip/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/aosip/prebuilt/common/bin/sysinit:system/bin/sysinit
+
+# Init file
+PRODUCT_COPY_FILES += \
+    vendor/aosip/prebuilt/common/etc/init.local.rc:root/init.aosip.rc
 
 # Don't export PS1 in /system/etc/mkshrc.
 PRODUCT_COPY_FILES += \
@@ -64,21 +70,34 @@ PRODUCT_COPY_FILES +=  \
 PRODUCT_COPY_FILES += \
     vendor/aosip/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
 
-# exFAT tools
-PRODUCT_PACKAGES += \
-    fsck.exfat \
-    mkfs.exfat
-
 # Storage manager
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_GENERIC_PROPERTIES += \
     ro.storage_manager.enabled=true
 
 # Media
 PRODUCT_GENERIC_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true
 
+# Set custom volume steps
+PRODUCT_GENERIC_PROPERTIES += \
+    ro.config.media_vol_steps=30 \
+    ro.config.bt_sco_vol_steps=30
+
+# Disable Rescue Party
+PRODUCT_GENERIC_PROPERTIES += \
+    persist.sys.disable_rescue=true
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
 # Don't compile SystemUITests
 EXCLUDE_SYSTEMUI_TESTS := true
+
+# exFAT tools
+PRODUCT_PACKAGES += \
+    fsck.exfat \
+    mkfs.exfat
 
 # Common overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/aosip/overlay/common
@@ -91,19 +110,3 @@ include vendor/aosip/config/packages.mk
 
 # Versioning
 include vendor/aosip/config/version.mk
-
-# Set custom volume steps
-PRODUCT_GENERIC_PROPERTIES += \
-    ro.config.media_vol_steps=30 \
-    ro.config.bt_sco_vol_steps=30
-
-# Disable Rescue Party
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.disable_rescue=true
-
-# Include SDCLANG definitions if it is requested and available
-ifeq ($(HOST_OS),linux)
-    ifneq ($(wildcard vendor/qcom/sdclang-4.0/),)
-        include vendor/aosip/sdclang/sdclang.mk
-    endif
-endif
