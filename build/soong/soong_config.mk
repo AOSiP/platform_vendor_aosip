LineageOS/android_vendor_lineage

soong: Add BOARD_USES_QCOM_HARDWARE
Change-Id: I925b137edeed9fa11bbe566002147b444328c661

build/soong/soong_config.mk
@@ -7,6 +7,7 @@ lineage_soong:
 	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
 	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
+	echo '    "Uses_qcom_hardware": $(if $(filter true,$(BOARD_USES_QCOM_HARDWARE)),true,false),'; \
 	echo '    "Uses_qcom_bsp_legacy": $(if $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)),true,false)'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)

AOSiP/platform_vendor_aosip

build/soong/soong_config.mk
@@ -36,6 +36,7 @@ aosip_soong:
 	echo '    "QCOMGPSPath": "$(call project-path-for,qcom-gps)",';  \
 	echo '    "QCOMMediaPath": "$(call project-path-for,qcom-media)",';  \
 	echo '    "QCOMSensorsPath": "$(call project-path-for,qcom-sensors)",';  \
-	echo '    "Uses_qcom_bsp_legacy": $(if $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)),true,false)'; \
+	echo '    "Uses_qcom_bsp_legacy": $(if $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)),true,false),'; \
+	echo '    "Uses_qcom_hardware": $(if $(filter true,$(BOARD_USES_QCOM_HARDWARE)),true,false)'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)