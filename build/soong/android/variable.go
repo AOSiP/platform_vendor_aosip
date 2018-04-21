63c544b988bb717c323d4477302f7fbdacb93aaa
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2018-04-21 09:14:16 -0400

    vendor:soong:Revert all unused variables
    Revert "vendor: Add TARGET_OMX_LEGACY_RESCALING flag"
    
    This reverts commit e9e293257c1aef2dd6339a6d1cae94fa5a62663c.
    
    Change-Id: I1925b5f77a38ebd9dcbdddf2210f6d6370177f49
    
    Revert "soong: Add QTI_flac_decoder config"
    
    This reverts commit f02e103dcb9142dc7b6c538e3460b50ec309111d.
    
    Change-Id: Ic7dfb6a1d3a4424d9dfc10fd63a3f7a3fbdf7272
    
    Revert "soong: Add Cant_reallocate_omx_buffers config"
    
    This reverts commit f954e9d075570af0db342c585b8f54c4642bb7c3.
    
    Change-Id: Ibe384455bdb519861dc3f41191e2d9d8ff8652ff
    
    Revert "aosip/vendor: Expose pathmaps to soong"
    
    This reverts commit fb301e4d43f6209d4d5363b2804bcdbe52bc5421.
    
    Change-Id: I3622334f308f9739fda5027320b862fc6381c980
    
    Revert "vendor/aosip: Add qcom board flags to soong"
    
    This reverts commit 58666bab18f10f4111b99f9d492682ba752e2e9a.
    
    Change-Id: I04c6c8bd63d44170ff4eac81583d56e539377c8a
    
    Revert "soong: handle qcom audio/display/media paths"
    
    This reverts commit dce1b5dca29dad118bdc912365a216c9bfb047c2.
    
    Change-Id: I4a3a1e5e39f53e67cea2428e73111dc4ccb78219
    
    Revert "soong_config: Add LIBART_IMG_BASE variable"
    
    This reverts commit ff9903213dceac4bacd133698be3e268a9a03380.
    
    Change-Id: I34a4fd6d858522fd3ab07a64d8eb2b2aef6e4216
    
    Revert "soong_config: Add TARGET_USES_MEDIA_EXTENSIONS variable"
    
    This reverts commit 90af2155dd70dd093ba904a9785c54f7b82f7e9e.
    
    Change-Id: Ic288bf4453bb23f986eb7eacd888905cbd2551de
    
    Revert "soong_config: Add TARGET_HAS_LEGACY_CAMERA_HAL1 variable"
    
    This reverts commit b98adfd079614ac5129bdf577b10b12c9d91eeea.
    
    Change-Id: I200eeb8792fe71dab965f2a6bbc8dfa5fd60fb7c
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,48 +1,13 @@
 package android_aosip
 type Product_variables struct {
-	Has_legacy_camera_hal1 struct {
-		Cflags []string
-	}
-	Uses_media_extensions struct {
-		Cflags []string
-	}
 	Needs_text_relocations struct {
 		Cppflags []string
-	}
-	BoardUsesQTIHardware struct {
-		Cflags []string
-		Cppflags []string
-	}
-	BoardUsesQCOMHardware struct {
-		Cflags []string
-		Cppflags []string
-	}
-	TargetUsesQCOMBsp struct {
-		Cflags []string
-		Cppflags []string
-	}
-	TargetUsesQCOMLegacyBsp struct {
-		Cflags []string
-		Cppflags []string
-	}
-	BoardUsesLegacyAlsa struct {
-		Cflags []string
-		Cppflags []string
-	}
-	Cant_reallocate_omx_buffers struct {
-		Cflags []string
 	}
 	Qcom_bsp_legacy struct {
 		Cflags []string
 	}
-	Qti_flac_decoder struct {
-		Cflags []string
-    }
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
-	}
-	Use_legacy_rescaling struct {
-		Cflags []string
 	}
 	Uses_qcom_bsp_legacy struct {
 		Cppflags []string
@@ -57,24 +22,10 @@ type Product_variables struct {
 }
 
 type ProductVariables struct {
-	Has_legacy_camera_hal1  *bool `json:",omitempty"`
-	Uses_media_extensions   *bool `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
-	QTIAudioPath            *string `json:",omitempty"`
-	QTIDisplayPath          *string `json:",omitempty"`
-	QTIMediaPath            *string `json:",omitempty"`
-	Uses_non_treble_camera  *bool `json:",omitempty"`
-	BoardUsesQTIHardware  *bool `json:",omitempty"`
-	BoardUsesQCOMHardware  *bool `json:",omitempty"`
-	TargetUsesQCOMBsp  *bool `json:",omitempty"`
-	TargetUsesQCOMLegacyBsp  *bool `json:",omitempty"`
-	BoardUsesLegacyAlsa  *bool `json:",omitempty"`
-	Cant_reallocate_omx_buffers *bool `json:",omitempty"`
 	Qcom_bsp_legacy         *bool `json:",omitempty"`
-	Qti_flac_decoder        *bool `json:",omitempty"`
-	Use_legacy_rescaling    *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -4,41 +4,13 @@ aosip_soong:
 	$(hide) (\
 	echo '{'; \
 	echo '"Aosip": {'; \
-	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
-	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
 	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
 	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
-	echo '    "Cant_reallocate_omx_buffers":  $(if $(filter omap4,$(TARGET_BOARD_PLATFORM)),true,false),'; \
 	echo '    "Qcom_bsp_legacy":  $(if $(filter msm7x27a msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),true,false),'; \
-	echo '    "Qti_flac_decoder":  $(if $(strip $(AUDIO_FEATURE_ENABLED_EXTN_FLAC_DECODER)),true,false),';  \
-	echo '    "Use_legacy_rescaling":  $(if $(strip $(TARGET_OMX_LEGACY_RESCALING)),true,false),';  \
 	echo '    "BoardUsesQTIHardware":  $(if $(BOARD_USES_QTI_HARDWARE),true,false),'; \
-	echo '    "Libart_img_base": "$(LIBART_IMG_BASE)",'; \
-	echo '    "QTIAudioPath":  "$(call project-path-for,qcom-audio)",'; \
-	echo '    "QTIDisplayPath":  "$(call project-path-for,qcom-display)",'; \
-	echo '    "QTIMediaPath":  "$(call project-path-for,qcom-media)",';  \
-	echo '    "Uses_non_treble_camera": $(if $(filter true,$(TARGET_USES_NON_TREBLE_CAMERA)),true,false),'; \
-	echo '    "BTVendorPath": "$(call project-path-for,bt-vendor)",'; \
-	echo '    "RILPath": "$(call project-path-for,ril)",'; \
-	echo '    "WLANPath": "$(call project-path-for,wlan)",'; \
-	echo '    "Target_shim_libs": "$(TARGET_LD_SHIM_LIBS)"'; \
-	echo '},'; \
-	echo '"Qualcomm": {'; \
-	echo '    "BoardUsesQTIHardware": $(if $(filter true,$(BOARD_USES_QTI_HARDWARE)),true,false),'; \
-	echo '    "BoardUsesQCOMHardware": $(if $(filter true,$(BOARD_USES_QCOM_HARDWARE)),true,false),'; \
-	echo '    "TargetUsesQCOMBsp": $(if $(filter true,$(TARGET_USES_QCOM_BSP)),true,false),'; \
-	echo '    "TargetUsesQCOMLegacyBsp": $(if $(filter true,$(TARGET_USES_QCOM_LEGACY_BSP)),true,false),'; \
-	echo '    "Target_shim_libs": "$(subst $(space),:,$(TARGET_LD_SHIM_LIBS))",'; \
-	echo '    "BoardUsesLegacyAlsa": $(if $(filter true,$(BOARD_USES_LEGACY_ALSA_AUDIO)),true,false),'; \
-	echo '    "QCOMAudioPath": "$(call project-path-for,qcom-audio)",'; \
-	echo '    "QCOMCameraPath": "$(call project-path-for,qcom-camera)",'; \
-	echo '    "QCOMDataservicesPath": "$(call project-path-for,qcom-dataservices)",';  \
-	echo '    "QCOMDisplayPath": "$(call project-path-for,qcom-display)",';  \
-	echo '    "QCOMGPSPath": "$(call project-path-for,qcom-gps)",';  \
-	echo '    "QCOMMediaPath": "$(call project-path-for,qcom-media)",';  \
-	echo '    "QCOMSensorsPath": "$(call project-path-for,qcom-sensors)",';  \
 	echo '    "Uses_qcom_bsp_legacy": $(if $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)),true,false),'; \
-	echo '    "Uses_qti_camera_device": $(if $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)),true,false)'; \
+	echo '    "Uses_qti_camera_device": $(if $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)),true,false),'; \
+	echo '    "Target_shim_libs": "$(subst $(space),:,$(TARGET_LD_SHIM_LIBS))"'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
