0bc626fd81578be169312fdfb57a906b5e7cfbb5
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2018-07-16 04:14:34 -0400

    vendor: Update bootanimation
    - big props to Moelle for the new boot
    - add 1312x2560 variant special to mata cuz mata-things
    
    Change-Id: I385e0573f172b99d99e95966e28d300c7e0fc9f3
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,2 +1,6 @@
+ifeq ($(AOSIP_BUILD), mata)
 PRODUCT_COPY_FILES += \
+    vendor/aosip/prebuilt/common/bootanimation/bootanimation1312.zip:system/media/bootanimation.zip
+else
     vendor/aosip/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+endif
