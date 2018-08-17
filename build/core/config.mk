7bb94a40dd08a6a79ba5dee558b1d3921ecdc9d9
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-17 09:20:21 -0700

    lineage: Ignore neverallows
    Change-Id: I35a39ec73c0047f93124f72249a6fa936fe0875a

--- build/core/config.mk
+++ build/core/config.mk
@@ -13,6 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
+
 # Rules for MTK targets
 include $(TOPDIR)vendor/aosip/build/core/mtk_target.mk
 
