5f3485fbbc693819b9bb34b962fa377f8c821df7
Harsh Shandilya <harsh@prjkt.io>
2018-09-01 11:15:12 +0530

    roomservice: Change default branch
    Change-Id: Ie7935178fd0bcfd673d5f02be0e3125c24e0af3d
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -31,7 +31,7 @@ DEBUG = False
 default_manifest = ".repo/manifest.xml"
 
 custom_local_manifest = ".repo/local_manifests/aosip_manifest.xml"
-custom_default_revision = "oreo-mr1"
+custom_default_revision = "pie"
 custom_dependencies = "aosip.dependencies"
 org_manifest = "devices"  # leave empty if org is provided in manifest
 org_display = "AOSiP-Devices"  # needed for displaying
