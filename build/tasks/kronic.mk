cb348c0bd29abe4c9a92b65311f19e875f6843a8
Harsh Shandilya <harsh@prjkt.io>
2018-09-03 19:16:00 +0530

    kronic: Don't create identical otapackages
    Saves space
    
    Change-Id: I9b0472c617d695330614baf8b41afb918cac0dac
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- build/tasks/kronic.mk
+++ build/tasks/kronic.mk
@@ -3,7 +3,7 @@ AOSIP_TARGET_PACKAGE := $(PRODUCT_OUT)/AOSiP-$(AOSIP_VERSION).zip
 .PHONY: otapackage kronic bacon
 otapackage: $(INTERNAL_OTA_PACKAGE_TARGET)
 kronic: otapackage
-	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(AOSIP_TARGET_PACKAGE)
+	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(AOSIP_TARGET_PACKAGE)
 	$(hide) $(MD5SUM) $(AOSIP_TARGET_PACKAGE) | cut -d ' ' -f1 > $(AOSIP_TARGET_PACKAGE).md5sum
 	@echo -e ""
 	@echo -e "${cya}Building ${bldcya}AOSiP ${txtrst}";
