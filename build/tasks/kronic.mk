47dc3bde1152cc847cf7fb54e1df6746cb9269a2
Harsh Shandilya <harsh@prjkt.io>
2018-09-04 21:46:54 +0530

    kronic: Remove color cruft in build completion message
    Doesn't render for me and literally who gives a fuck
    
    Change-Id: I042ac40ca5ff6c2ff74c21eef6c84402d94b9a7d
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- build/tasks/kronic.mk
+++ build/tasks/kronic.mk
@@ -14,9 +14,9 @@ kronic: otapackage
 	@echo -e ${CL_GRN}"           ▐█ ▪▐▌▐█▌.▐▌▐█▄▪▐█▐█▌▐█▪·•          "
 	@echo -e ${CL_GRN}"            ▀  ▀  ▀█▄▀▪ ▀▀▀▀ ▀▀▀.▀             "
 	@echo -e ""
-	@echo -e ${CL_MAG}"zip: "${CL_CYN} $(AOSIP_TARGET_PACKAGE)${CL_RST}
-	@echo -e ${CL_MAG}"md5: "${CL_CYN}" `cat $(AOSIP_TARGET_PACKAGE).md5sum | cut -d ' ' -f 1`"${CL_RST}
-	@echo -e ${CL_MAG}"size:"${CL_CYN}" `ls -lah $(AOSIP_TARGET_PACKAGE) | cut -d ' ' -f 5`"${CL_RST}
+	@echo -e "zip: "$(AOSIP_TARGET_PACKAGE)
+	@echo -e "md5: `cat $(AOSIP_TARGET_PACKAGE).md5sum | cut -d ' ' -f 1`"
+	@echo -e "size:`ls -lah $(AOSIP_TARGET_PACKAGE) | cut -d ' ' -f 5`"
 	@echo -e ""
 
 bacon: kronic
