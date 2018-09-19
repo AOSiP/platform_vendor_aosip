649ca78a0528d92dee81044a8491f8e103354056
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-18 21:53:24 -0700

    aosip: Dynamically generate kernel headers using lineage generator
    Add a soong vendor plugin for kernel config variables so we can use
    these in go.
    
    Change-Id: Id31f2be8fcc5aba2d965dbe815edaaf1d28279c6
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- config/BoardConfigAOSiP.mk
+++ config/BoardConfigAOSiP.mk
@@ -3,3 +3,5 @@ include vendor/aosip/config/BoardConfigKernel.mk
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/aosip/config/BoardConfigQcom.mk
 endif
+
+include vendor/aosip/config/BoardConfigSoong.mk
