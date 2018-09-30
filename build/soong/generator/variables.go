1e340b0e0cdd90eaf1367f4811f5a1339b509c11
Akhil Narang <akhilnarang.1999@gmail.com>
2018-09-30 12:42:48 +0530

    [DNM]aosip: s/lineage/aosip/g build/soong
      * Squash into the original picks
    
    Change-Id: Id5043231eece615312e5fa9a9458eb426659c2cb
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- build/soong/generator/variables.go
+++ build/soong/generator/variables.go
@@ -6,12 +6,12 @@ import (
 	"android/soong/android"
 )
 
-func lineageExpandVariables(ctx android.ModuleContext, in string) string {
-	lineageVars := ctx.Config().VendorConfig("lineageVarsPlugin")
+func aosipExpandVariables(ctx android.ModuleContext, in string) string {
+	aosipVars := ctx.Config().VendorConfig("aosipVarsPlugin")
 
 	out, err := android.Expand(in, func(name string) (string, error) {
-		if lineageVars.IsSet(name) {
-			return lineageVars.String(name), nil
+		if aosipVars.IsSet(name) {
+			return aosipVars.String(name), nil
 		}
 		// This variable is not for us, restore what the original
 		// variable string will have looked like for an Expand
