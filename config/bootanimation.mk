285a78e271234d2ce8ad9433b0e138075bce83db
Akhil Narang <akhilnarang.1999@gmail.com>
2018-12-22 13:46:00 +0530

    aosip: back to old bootanimation for now
    Many people preffered that one
    This reverts commits 5190183889502ece181cda318f63442e5089e4a8 and 0bc626fd81578be169312fdfb57a906b5e7cfbb5
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    Change-Id: Iee44d81d3cf2de2ab039a558448bd9cc7abefed6

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,7 +1,2 @@
-ifeq ($(AOSIP_BUILD), mata)
-PRODUCT_COPY_FILES += \
-    vendor/aosip/prebuilt/common/bootanimation/bootanimation1312.zip:system/media/bootanimation.zip
-else
 PRODUCT_COPY_FILES += \
     vendor/aosip/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
-endif
