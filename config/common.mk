6086c9f28ff28732dc57c002acc260970624134d
Alex Cruz <du.alexcruz@gmail.com>
2018-12-29 00:53:31 -0600

    Compile WeatherClient from source
    Change-Id: Ied18d6547ac864c16b266b396e6f07a121a62e0b
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -69,6 +69,11 @@ $(foreach f,$(wildcard vendor/aosip/prebuilt/common/etc/init/*.rc),\
 PRODUCT_COPY_FILES +=  \
     vendor/aosip/prebuilt/common/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
 
+# Weather client
+PRODUCT_COPY_FILES += \
+    vendor/aosip/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
+    vendor/aosip/etc/default-permissions/org.pixelexperience.weather.client.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml
+
 # Storage manager
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.storage_manager.enabled=true
@@ -110,6 +115,3 @@ include vendor/aosip/config/accents.mk
 
 # Themes
 include vendor/aosip/config/themes.mk
-
-# Weather
-include vendor/aosip/config/weather.mk
--- config/weather.mk
+++
@@ -1,4 +0,0 @@
-PRODUCT_COPY_FILES += \
-    vendor/aosip/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
-    vendor/aosip/etc/default-permissions/org.pixelexperience.weather.client.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml
-
