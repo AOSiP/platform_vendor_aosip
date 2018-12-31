cb26bbdf18283f9102f63d693872d1478d13c2a9
Harsh Shandilya <msfjarvis@gmail.com>
2018-12-31 23:20:32 +0530

    extract_utils: Fix path to cdex extractor
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Change-Id: I94e1e3a6a1e53db327b0d84665d37779cd6400a7

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -788,7 +788,7 @@ function oat2dex() {
     fi
 
     if [ -z "$CDEXCONVERTER" ]; then
-        export CDEXCONVERTER="$LINEAGE_ROOT"/vendor/lineage/build/tools/"$HOST"/compact_dex_converter
+        export CDEXCONVERTER="$AOSIP_ROOT"/vendor/aosip/build/tools/"$HOST"/compact_dex_converter
     fi
 
     # Extract existing boot.oats to the temp folder
