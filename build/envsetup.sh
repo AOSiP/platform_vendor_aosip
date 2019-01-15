4865154f29e6fe430504e939cf0eee331b0913e2
Harsh Shandilya <msfjarvis@gmail.com>
2019-01-15 17:24:57 +0530

    build: Don't reload vendorsetup again
    build/make does this already
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Change-Id: I82b6a043f47aeea407e5aae3c39d4ce4ff50c4ca

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -76,16 +76,6 @@ if [ "x$SHELL" != "x/bin/bash" ]; then
     esac
 fi
 
-# Execute the contents of any vendorsetup.sh files we can find.
-for f in `test -d device && find -L device -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort` \
-         `test -d vendor && find -L vendor -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort` \
-         `test -d product && find -L product -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort`
-do
-    echo "including $f"
-    . $f
-done
-unset f
-
 # Make using all available CPUs
 function mka() {
     m -j "$@"
