284b2b250f6f4e1032e443fc7072e91108b46b25
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2019-02-16 10:47:29 -0500

    vendor: Properly add TurboPrebuilt
    Change-Id: I886a5763a8adf08d12bca682ec5a1f8d6acd1828
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- prebuilt/common/priv-app/Android.mk
+++ prebuilt/common/priv-app/Android.mk
@@ -14,6 +14,17 @@
 
 LOCAL_PATH:= $(call my-dir)
 
+#Turbo
+include $(CLEAR_VARS)
+LOCAL_MODULE := TurboPrebuilt
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := TurboPrebuilt/TurboPrebuilt.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
+
 #Wellbeing
 include $(CLEAR_VARS)
 LOCAL_MODULE := WellbeingPrebuilt
