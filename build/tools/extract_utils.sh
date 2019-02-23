410b9f71bd7e79343450a3a95b76b5ca366860c8
Akhil Narang <akhilnarang.1999@gmail.com>
2019-02-23 20:22:19 +0530

    aosip: extract_utils: update copyright headers
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    Change-Id: Idd6ec7aca4ea7414e0600a6a2b096bff3441a52e
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -549,14 +549,14 @@ function write_header() {
         if [ $YEAR -eq 2017 ]; then
             printf "# Copyright (C) 2017 The LineageOS Project\n" >> $1
         elif [ $INITIAL_COPYRIGHT_YEAR -eq $YEAR ]; then
-            printf "# Copyright (C) $YEAR The LineageOS Project\n" >> $1
+            printf "# Copyright (C) $YEAR AOSiP\n" >> $1
         elif [ $INITIAL_COPYRIGHT_YEAR -le 2017 ]; then
-            printf "# Copyright (C) 2017-$YEAR The LineageOS Project\n" >> $1
+            printf "# Copyright (C) 2017-$YEAR AOSiP\n" >> $1
         else
-            printf "# Copyright (C) $INITIAL_COPYRIGHT_YEAR-$YEAR The LineageOS Project\n" >> $1
+            printf "# Copyright (C) $INITIAL_COPYRIGHT_YEAR-$YEAR AOSiP\n" >> $1
         fi
     else
-        printf "# Copyright (C) $YEAR The LineageOS Project\n" > $1
+        printf "# Copyright (C) $YEAR AOSiP\n" > $1
     fi
 
     cat << EOF >> $1
