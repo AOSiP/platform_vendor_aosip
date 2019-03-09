759ccef847883681547a260599929757cef33a6e
Erfan Abdi <erfangplus@gmail.com>
2019-03-09 12:03:41 +0200

    soong_config: Add flag for devices use metadata as FDE key
    Change-Id: I5f70280e72613a8f850cb3263c45e95e80cb8b3c
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -17,9 +17,10 @@ $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PRO
 $(call add_json_str,  Target_shim_libs,                      $(TARGET_LD_SHIM_LIBS))
 $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
 $(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),,true))
-$(call add_json_bool, Uses_nvidia_enhancements,              $(filter TRUE,$(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)))
-$(call add_json_bool, Uses_qcom_bsp_legacy,                  $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
-$(call add_json_bool, Uses_qti_camera_device,                $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
+$(call add_json_bool, Uses_metadata_as_fde_key, $(filter true,$(TARGET_USES_METADATA_AS_FDE_KEY)))
+$(call add_json_bool, Uses_nvidia_enhancements, $(filter TRUE,$(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)))
+$(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
+$(call add_json_bool, Uses_qti_camera_device, $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END
