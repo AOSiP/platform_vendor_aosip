fbc5c0345b2f59073010777ef419cf3b2a98feab
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2019-07-22 23:55:35 -0400

    vendor: Clean up common.mk
    Change-Id: I81d6cb79c71dab9fa79bb0b7899051a3b006ed73
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -1,21 +1,24 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    keyguard.no_require_sim=true \
     dalvik.vm.debug.alloc=0 \
-    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
-    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-    ro.error.receiver.system.apps=com.google.android.gms \
-    ro.setupwizard.enterprise_mode=1 \
-    ro.com.android.dataroaming=false \
+    keyguard.no_require_sim=true \
+    media.recorder.show_manufacturer_and_model=true \
+    net.tethering.noprovisioning=true \
+    persist.sys.disable_rescue=true \
     ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
-    ro.com.android.dateformat=MM-dd-yyyy \
     ro.build.selinux=1 \
-    ro.carrier=unknown
-
-# Allow tethering without provisioning app
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += net.tethering.noprovisioning=true
-
+    ro.carrier=unknown \
+    ro.com.android.dataroaming=false \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.config.bt_sco_vol_steps=30 \
+    ro.config.media_vol_steps=30 \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.opa.eligible_device=true \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.storage_manager.enabled=true \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -23,24 +26,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 else
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
-endif
-
-# Include SDCLANG definitions if it is requested and available
-ifeq ($(HOST_OS),linux)
-    ifneq ($(wildcard vendor/qcom/sdclang-4.0/),)
-        include vendor/aosip/sdclang/sdclang.mk
-    endif
-endif
-
-# LatinIME gesture typing
-ifeq ($(TARGET_ARCH),arm64)
-PRODUCT_COPY_FILES += \
-    vendor/aosip/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
-    vendor/aosip/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
-else
-PRODUCT_COPY_FILES += \
-    vendor/aosip/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
-    vendor/aosip/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 endif
 
 # Backup Tool
@@ -57,84 +42,82 @@ PRODUCT_COPY_FILES += \
     vendor/aosip/prebuilt/common/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
 endif
 
-PRODUCT_COPY_FILES += \
-    vendor/aosip/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/aosip/prebuilt/common/bin/sysinit:system/bin/sysinit
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
 
-# Don't export PS1 in /system/etc/mkshrc.
+# Configs
 PRODUCT_COPY_FILES += \
-    vendor/aosip/prebuilt/common/etc/mkshrc:system/etc/mkshrc
+    vendor/aosip/prebuilt/common/etc/sysconfig/aosip-power-whitelist.xml:system/etc/sysconfig/aosip-power-whitelist.xml \
+    vendor/aosip/prebuilt/common/etc/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml \
+    vendor/aosip/prebuilt/common/etc/sysconfig/turbo.xml:system/etc/sysconfig/turbo.xml \
+    vendor/aosip/prebuilt/common/etc/sysconfig/org.pixelexperience.weather.client-default.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml
 
 # Copy all AOSiP-specific init rc files
 $(foreach f,$(wildcard vendor/aosip/prebuilt/common/etc/init/*.rc),\
 	$(eval PRODUCT_COPY_FILES += $(f):system/etc/init/$(notdir $f)))
 
-# Configs
+# Don't compile SystemUITests
+EXCLUDE_SYSTEMUI_TESTS := true
+
+# Don't export PS1 in /system/etc/mkshrc.
 PRODUCT_COPY_FILES += \
-    vendor/aosip/prebuilt/common/etc/sysconfig/aosip-power-whitelist.xml:system/etc/sysconfig/aosip-power-whitelist.xml \
-    vendor/aosip/prebuilt/common/etc/sysconfig/aosip-privapp-permissions.xml:system/etc/permissions/aosip-privapp-permissions.xml \
-    vendor/aosip/prebuilt/common/etc/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml \
-    vendor/aosip/prebuilt/common/etc/sysconfig/turbo.xml:system/etc/sysconfig/turbo.xml \
-    vendor/aosip/prebuilt/common/etc/sysconfig/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
-    vendor/aosip/prebuilt/common/etc/sysconfig/org.pixelexperience.weather.client-default.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml
+    vendor/aosip/prebuilt/common/etc/mkshrc:system/etc/mkshrc
 
-# Google Assistant
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.opa.eligible_device=true
-
-# Google permissions
-PRODUCT_COPY_FILES += \
-    vendor/aosip/prebuilt/common/etc/permissions/privapp-permissions-elgoog.xml:system/etc/permissions/privapp-permissions-elgoog.xml
-
-# Do not include art debug targets
+# Don't include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
+# Include SDCLANG definitions if it is requested and available
+ifeq ($(HOST_OS),linux)
+    ifneq ($(wildcard vendor/qcom/sdclang-4.0/),)
+        include vendor/aosip/sdclang/sdclang.mk
+    endif
+endif
+
+# Init
+PRODUCT_COPY_FILES += \
+    vendor/aosip/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/aosip/prebuilt/common/bin/sysinit:system/bin/sysinit
+
+# LatinIME gesture typing
+ifeq ($(TARGET_ARCH),arm64)
+PRODUCT_COPY_FILES += \
+    vendor/aosip/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
+    vendor/aosip/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
+else
+PRODUCT_COPY_FILES += \
+    vendor/aosip/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
+    vendor/aosip/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+endif
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/aosip/prebuilt/common/etc/permissions/aosip-privapp-permissions.xml:system/etc/permissions/aosip-privapp-permissions.xml \
+    vendor/aosip/prebuilt/common/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
+    vendor/aosip/prebuilt/common/etc/permissions/privapp-permissions-elgoog.xml:system/etc/permissions/privapp-permissions-elgoog.xml
 
 # Strip the local variable table and the local variable type table to reduce
 # the size of the system image. This has no bearing on stack traces, but will
 # leave less information available via JDWP.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
-# Storage manager
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.storage_manager.enabled=true
-
-# Media
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    media.recorder.show_manufacturer_and_model=true
-
-# Set custom volume steps
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.config.media_vol_steps=30 \
-    ro.config.bt_sco_vol_steps=30
-
-# Disable Rescue Party
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    persist.sys.disable_rescue=true
-
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images
-
-# Don't compile SystemUITests
-EXCLUDE_SYSTEMUI_TESTS := true
-
 # Common overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/aosip/overlay/common
-
-# Bootanimation
-include vendor/aosip/config/bootanimation.mk
-
-# Packages
-include vendor/aosip/config/packages.mk
-
-# Versioning
-include vendor/aosip/config/version.mk
 
 # Accents
 include vendor/aosip/config/accents.mk
 
-# Themes
-include vendor/aosip/config/themes.mk
+# Bootanimation
+include vendor/aosip/config/bootanimation.mk
 
 # GSans font
 include vendor/aosip/config/fonts.mk
+
+# Packages
+include vendor/aosip/config/packages.mk
+
+# Themes
+include vendor/aosip/config/themes.mk
+
+# Versioning
+include vendor/aosip/config/version.mk
