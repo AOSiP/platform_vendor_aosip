12ff05b517539f978d2292fc75d11fe0b9b3ce50
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-08-13 09:22:34 -0300

    vendor: Import modded Longshot app
    Change-Id: I1f1b575afa4c13ed776d8f21047077d57a5335a4
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -28,6 +28,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
+# Long screenshot
+PRODUCT_PACKAGES += \
+    Longshot
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/aosip/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
