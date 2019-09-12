1abcfbd641a1174966a80bce27dce55ac2aa92b8
Akhil Narang <akhilnarang.1999@gmail.com>
2019-09-12 10:47:33 +0000

    Revert "vendor: Import modded Longshot app"
    This reverts commit 12ff05b517539f978d2292fc75d11fe0b9b3ce50.
    
    Reason for revert: Bye bye
    
    Change-Id: I64ed5a54a49d767bfaca23a5dc3cd13d883654d6

--- config/common.mk
+++ config/common.mk
@@ -28,10 +28,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
-# Long screenshot
-PRODUCT_PACKAGES += \
-    Longshot
-
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/aosip/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
