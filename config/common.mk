20a3b23e7312acc4c062887b459e10af24ff6108
Dan Pasanen <dan.pasanen@gmail.com>
2019-10-23 08:35:32 -0500

    aosip: use AB_OTA_PARTITIONS flag for including backuptool_ab
    * Newer pixel devices get AB_OTA_UPDATER enabled from
      build/make/target/board/BoardConfigMainlineCommon.mk which
      is not included in time for this check.
    
    Change-Id: I1679709b7178398ffaa62ad14a6d042ff377fd5a
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -34,7 +34,7 @@ PRODUCT_COPY_FILES += \
     vendor/aosip/prebuilt/common/bin/50-base.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-base.sh \
     vendor/aosip/prebuilt/common/bin/blacklist:$(TARGET_COPY_OUT_SYSTEM)/addon.d/blacklist
 
-ifeq ($(AB_OTA_UPDATER),true)
+ifneq ($(AB_OTA_PARTITIONS),)
 PRODUCT_COPY_FILES += \
     vendor/aosip/prebuilt/common/bin/backuptool_ab.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.sh \
     vendor/aosip/prebuilt/common/bin/backuptool_ab.functions:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.functions \
