5ac2b68ea4d7ae601d3b6ead6d9117a0af7d86d5
Akhil Narang <akhilnarang.1999@gmail.com>
2020-02-05 21:25:45 +0530

    aosip: update gerrit URL
    Change-Id: I96d4fdb579dd450bbf2988c563edf95fe8a79e12
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    (cherry picked from commit 5933974e7c89b9940e64ee27869d751876e03b53)

--- build/tools/repopick.py
+++ build/tools/repopick.py
@@ -152,7 +152,7 @@ def fetch_query(remote_url, query):
 
 if __name__ == '__main__':
     # Default to AOSiP Gerrit
-    default_gerrit = 'https://review.aosiprom.com'
+    default_gerrit = 'https://review.aosip.dev'
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent('''\
         repopick.py is a utility to simplify the process of cherry picking
