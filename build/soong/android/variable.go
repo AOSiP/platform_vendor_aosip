fa163d95af315eb747875caf89273c8caf273b4d
Paul Keith <javelinanddart@gmail.com>
2020-05-06 19:10:03 -0700

    aosip: Add flag for skipping EV_REL input in recovery
    * Needed now that recovery is in bp
    
    Change-Id: I625ac22e79912cbdea328eba12dc1dd16448e7a8

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -14,6 +14,9 @@ type Product_variables struct {
 		Overrides []string
 		Shared_libs []string
 	}
+	Recovery_skip_ev_rel_input struct {
+		Cflags []string
+	}
 	Should_wait_for_qsee struct {
 		Cflags []string
 	}
@@ -63,6 +66,7 @@ type ProductVariables struct {
 	Apply_msm8974_1440p_egl_workaround  *bool `json:",omitempty"`
 	Bootloader_message_offset  *int `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
+	Recovery_skip_ev_rel_input  *bool `json:",omitempty"`
 	Should_wait_for_qsee  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Supports_extended_compress_format  *bool `json:",omitempty"`
