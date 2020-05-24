e9a26633fc9cb6a3b04961b3eece5e9499102ae7
Akhil Narang <me@akhilnarang.dev>
2020-05-24 12:53:59 +0530

    extract_utils: fix stuff Brett broke 3 years ago in e68e4cb2f
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    Change-Id: I7bbb00158961d1432e39497734fa7eafb0b22786

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -1477,7 +1477,7 @@ function extract() {
             # If OTA is block based, extract it.
             elif [ -a "$DUMPDIR"/system.new.dat ]; then
                 echo "Converting system.new.dat to system.img"
-                python "$AOSIP_ROOT"/vendor/AOSIP/build/tools/sdat2img.py "$DUMPDIR"/system.transfer.list "$DUMPDIR"/system.new.dat "$DUMPDIR"/system.img 2>&1
+                python "$AOSIP_ROOT"/vendor/aosip/build/tools/sdat2img.py "$DUMPDIR"/system.transfer.list "$DUMPDIR"/system.new.dat "$DUMPDIR"/system.img 2>&1
                 rm -rf "$DUMPDIR"/system.new.dat "$DUMPDIR"/system
                 mkdir "$DUMPDIR"/system "$DUMPDIR"/tmp
                 echo "Requesting sudo access to mount the system.img"
