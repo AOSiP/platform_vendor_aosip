0f9e325dc6baf3e3d9f19ee24e7deaeb51c7ea92
Jan Altensen <info@stricted.net>
2020-09-11 21:41:08 +0200

    kronic: switch to toybox md5sum
     * fixes "error: MD5SUM is obsolete."
    
    Change-Id: I38e167e24e9eb5596a1e8e2f2d915f648bc1283c

--- build/tasks/kronic.mk
+++ build/tasks/kronic.mk
@@ -1,9 +1,11 @@
 AOSIP_TARGET_PACKAGE := $(PRODUCT_OUT)/AOSiP-$(AOSIP_VERSION).zip
 
+MD5 := prebuilts/build-tools/path/$(HOST_OS)-x86/md5sum
+
 .PHONY: kronic bacon
 kronic: otapackage
 	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(AOSIP_TARGET_PACKAGE)
-	$(hide) $(MD5SUM) $(AOSIP_TARGET_PACKAGE) | cut -d ' ' -f1 > $(AOSIP_TARGET_PACKAGE).md5sum
+	$(hide) $(MD5) $(AOSIP_TARGET_PACKAGE) | cut -d ' ' -f1 > $(AOSIP_TARGET_PACKAGE).md5sum
 	@echo -e ""
 	@echo -e "${cya}Building ${bldcya}AOSiP ${txtrst}";
 	@echo -e ""
