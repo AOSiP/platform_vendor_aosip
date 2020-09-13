d3af32a022ec308cb0fd4554231f385dc79b1845
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2020-09-13 14:58:05 -0400

    vendor: fix build breakages
    Change-Id: I1da600c160334c852eb89b04e102f7ce1fcbbad5

--- config/common.mk
+++ config/common.mk
@@ -70,9 +70,6 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI \
     Launcher3QuickStep
 
-# Don't compile SystemUITests
-EXCLUDE_SYSTEMUI_TESTS := true
-
 # Don't include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
@@ -83,6 +80,100 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/addon.d/50-base.sh \
+    system/bin/curl \
+    system/bin/getcap \
+    system/bin/setcap \
+    system/etc/init/aosip-adb.rc \
+    system/etc/init/aosip-iosched.rc \
+    system/etc/init/aosip-radio.rc \
+    system/etc/init/aosip-ssh.rc \
+    system/etc/init/aosip-system.rc \
+    system/etc/init/aosip-updates.rc \
+    system/etc/selective-spn-conf.xml \
+    system/etc/sensitive_pn.xml \
+    system/etc/spn-conf.xml \
+    system/etc/sysconfig/backup.xml \
+    system/etc/sysconfig/dialer_experience.xml \
+    system/etc/sysconfig/turbo.xml \
+    system/lib/libRSSupport.so \
+    system/lib/libblasV8.so \
+    system/lib/librsjni.so \
+    system/lib/libsepol.so \
+    system/lib64/libRSSupport.so \
+    system/lib64/libblasV8.so \
+    system/lib64/librsjni.so \
+    system/lib64/libsepol.so \
+    system/bin/fsck.exfat \
+system/bin/fsck.ntfs \
+system/bin/mkfs.exfat \
+system/bin/mkfs.ntfs \
+system/bin/mount.ntfs \
+system/etc/bash/bash_logout \
+system/etc/bash/bashrc \
+system/etc/bash/inputrc \
+system/etc/vimrc \
+system/lib/lib7z.so \
+system/lib64/lib7z.so \
+system/lib64/libfuse-lite.so \
+system/lib64/libncurses.so \
+system/lib64/libntfs-3g.so \
+system/usr/share/vim/autoload/dist/ft.vim \
+system/usr/share/vim/autoload/spacehi.vim \
+system/usr/share/vim/colors/default.vim \
+system/usr/share/vim/colors/desert.vim \
+system/usr/share/vim/doc/editing.txt \
+system/usr/share/vim/doc/help.txt \
+system/usr/share/vim/doc/intro.txt \
+system/usr/share/vim/doc/motion.txt \
+system/usr/share/vim/doc/options.txt \
+system/usr/share/vim/doc/scroll.txt \
+system/usr/share/vim/doc/tags \
+system/usr/share/vim/doc/term.txt \
+system/usr/share/vim/doc/version8.txt \
+system/usr/share/vim/filetype.vim \
+system/usr/share/vim/ftoff.vim \
+system/usr/share/vim/indent.vim \
+system/usr/share/vim/indoff.vim \
+system/usr/share/vim/plugin/matchparen.vim \
+system/usr/share/vim/scripts.vim \
+system/usr/share/vim/syntax/awk.vim \
+system/usr/share/vim/syntax/c.vim \
+system/usr/share/vim/syntax/conf.vim \
+system/usr/share/vim/syntax/config.vim \
+system/usr/share/vim/syntax/context.vim \
+system/usr/share/vim/syntax/cpp.vim \
+system/usr/share/vim/syntax/css.vim \
+system/usr/share/vim/syntax/diff.vim \
+system/usr/share/vim/syntax/doxygen.vim \
+system/usr/share/vim/syntax/dtd.vim \
+system/usr/share/vim/syntax/gitcommit.vim \
+system/usr/share/vim/syntax/help.vim \
+system/usr/share/vim/syntax/html.vim \
+system/usr/share/vim/syntax/java.vim \
+system/usr/share/vim/syntax/javascript.vim \
+system/usr/share/vim/syntax/logcat.vim \
+system/usr/share/vim/syntax/lua.vim \
+system/usr/share/vim/syntax/manual.vim \
+system/usr/share/vim/syntax/markdown.vim \
+system/usr/share/vim/syntax/pod.vim \
+system/usr/share/vim/syntax/sh.vim \
+system/usr/share/vim/syntax/syncolor.vim \
+system/usr/share/vim/syntax/synload.vim \
+system/usr/share/vim/syntax/syntax.vim \
+system/usr/share/vim/syntax/vb.vim \
+system/usr/share/vim/syntax/vim.vim \
+system/usr/share/vim/syntax/xml.vim \
+system/xbin/7z \
+system/xbin/bash \
+system/xbin/htop \
+system/xbin/pigz \
+system/xbin/unrar \
+system/xbin/vim \
+system/xbin/zip
+
 
 # Overlays
 include vendor/overlay/overlays.mk
