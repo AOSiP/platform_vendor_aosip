ba6952ef2d6470f60d8e479b29863d8a5763ff4a
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-10-02 11:50:22 +0530

    aosip: Apply 6a992d459cf2e0d7658d014a939975cc2d0b7b49 to few more places
    Change-Id: I1b6b626e2a928d6426962e8ce6f750122a50bd35
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -183,10 +183,10 @@ ifeq ($(TARGET_KERNEL_CLANG_COMPILE),true)
     ifneq ($(TARGET_KERNEL_CLANG_VERSION),)
         ifeq ($(TARGET_KERNEL_CLANG_VERSION),latest)
             # Set the latest version of clang
-            KERNEL_CLANG_VERSION := $(shell ls -d $(BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/clang-r* | xargs -n 1 basename | tail -1)
+            KERNEL_CLANG_VERSION := $(shell ls -d $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)/clang-r* | xargs -n 1 basename | tail -1)
         else
             # Find the clang-* directory containing the specified version
-            KERNEL_CLANG_VERSION := $(shell find $(BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/ -name AndroidVersion.txt -exec grep -l $(TARGET_KERNEL_CLANG_VERSION) "{}" \; | sed -e 's|/AndroidVersion.txt$$||g;s|^.*/||g')
+            KERNEL_CLANG_VERSION := $(shell find $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)/ -name AndroidVersion.txt -exec grep -l $(TARGET_KERNEL_CLANG_VERSION) "{}" \; | sed -e 's|/AndroidVersion.txt$$||g;s|^.*/||g')
         endif
     else
         # Use the default version of clang if TARGET_KERNEL_CLANG_VERSION hasn't been set by the device config
